e4d360cd58ca023e09c20408272c9138c8ccb3c3
Dario Chamorro <dario.chamorro.v@gmail.com>
2017-10-03 22:21:20 -0500

    View model implementado

--- app/src/main/java/codemakers/archdeno/MainActivity.kt
+++ app/src/main/java/codemakers/archdeno/MainActivity.kt
@@ -1,15 +1,24 @@
 package codemakers.archdeno
 
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn.setOnClickListener{  txtInfo.text = "Hola"}
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        txtInfo.text = viewModel.info
+
+        btn.setOnClickListener{
+            viewModel.info = "Hola"
+            txtInfo.text = viewModel.info}
     }
 }
---
+++ app/src/main/java/codemakers/archdeno/MainViewModel.kt
@@ -0,0 +1,12 @@
+package codemakers.archdeno
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * Created by darfe on 3/10/2017.
+ */
+
+class MainViewModel: ViewModel(){
+
+    var info = "Hello World"
+}
